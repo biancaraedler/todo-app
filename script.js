e74bdbaa925d397500af00a0afe7850680f971e2
Bianca Raedler <raedler.bianca@gmail.com>
2022-03-21 15:50:53 +0100

    feat: loead todos and render todos

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+const addTodoBtn = document.querySelector("#add-todo-btn");
+const newTodoInput = document.querySelector("#create-new-todo");
 const todoList = document.querySelector("#todo-list");
-const showTodo = document.querySelector("#show-todo-api");
 
-fetch("http://localhost:4730/todos")
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-  })
-  .then((jsonData) => {
-    console.log(jsonData);
+let todos = []; // da speichere ich die todos von der Api rein mit todos = todosFromApi;
+
+function loadTodos() {
+  fetch("http://localhost:4730/todos")
+    .then((res) => res.json())
+    .then((todosFromApi) => {
+      // hier habe ich die todos zurückbekommen
+      todos = todosFromApi;
+      renderTodos();
+    });
+}
+
+function renderTodos() {
+  // hier zeichne ich mein todos oder create
+  todoList.innerHTML = ""; // den inhalt erstmal gelöscht
+  todos.forEach((todo) => {
+    const newLi = document.createElement("li");
+    const text = document.createTextNode(todo.description);
+    newLi.appendChild(text);
+
+    todoList.appendChild(newLi);
   });
+}
+
+loadTodos(); //hier hab ich sie aufgerufen
